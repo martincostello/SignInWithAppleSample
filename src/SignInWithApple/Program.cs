386806962362852117faefd00ed0f2174f07f577
martincostello <martin@martincostello.com>
2021-06-06 11:09:16 +0100

    Use minimal hosting
    Use new ASP.NET Core WebApplication APIs.

--- src/SignInWithApple/Program.cs
+++ src/SignInWithApple/Program.cs
@@ -1,19 +1,55 @@
 // Copyright (c) Martin Costello, 2019. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using MartinCostello.SignInWithApple;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
 
-namespace MartinCostello.SignInWithApple
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignInWithApple();
+builder.Services.AddMvc(options => options.Filters.Add(new RequireHttpsAttribute()));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults((webBuilder) => webBuilder.UseStartup<Startup>())
-                .TryConfigureAzureKeyVault();
-    }
+    app.UseDeveloperExceptionPage();
+    IdentityModelEventSource.ShowPII = true;
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error")
+       .UseStatusCodePages();
+
+    app.UseHsts()
+       .UseHttpsRedirection();
+}
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".webmanifest"] = "application/manifest+json";
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider,
+    DefaultContentType = "application/json",
+    ServeUnknownFileTypes = true, // Required to serve the files in the .well-known folder
+});
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapAuthenticationRoutes();
+});
+
+app.Run();
